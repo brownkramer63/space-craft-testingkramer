56582cda0528c5a1b0381a1fb149a0deff6bf677
Kramer Brown <brownkramer63@gmail.com>
2022-12-19 10:43:24 -0600

    testing in action

---
+++ src/test/java/CreatePlayerServiceImplTest.java
@@ -0,0 +1,21 @@
+import com.cydeo.spacecraft.service.impl.CreatePlayerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CreatePlayerServiceImplTest {
+
+    private CreatePlayerServiceImpl createPlayerService;
+
+    @BeforeEach
+    public void setUp(){
+        createPlayerService = new CreatePlayerServiceImpl();
+    }
+
+    @Test
+    public void should_create_player_with_big_bomb_boost_type(){
+
+    }
+
+
+
+}
